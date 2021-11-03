cf2697e002c737e27ebdc5a130f98aa8728fff46
Addin Satria <54827832+AddinDev@users.noreply.github.com>
2021-11-03 08:07:49 +0000

    search news + spam

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import os
 import requests
 import json
 import random
+from replit import db
 
 from keep_alive import keep_alive
 
+# Prefix
+news_limit  = 5
+
+# Tokens \ Keys
 token = os.environ['TOKEN']
 newsKey = os.environ['newsKey']
+
 
 client = discord.Client()
 
@@ -33,14 +39,23 @@ def get_quran(index):
   text = surah['name'] + " - " + surah['englishName'] + " - " + surah['englishNameTranslation'] + " - " + str(surah['numberOfAyahs']) + " ayahs"
   return(text)
 
-def getNews():
+def get_news():
   url = 'https://newsapi.org/v2/top-headlines?country=id&apiKey=' + newsKey
   response = requests.get(url)
   json_data = json.loads(response.text)
   news_data = json_data["articles"]
   news_list = []
+  for index in range(news_limit):
+  	news_list.append(news_data[index])
+  return news_list
 
-  for index in range(5):
+def search_news(word):
+  url = 'https://newsapi.org/v2/everything?q=' + word + '&sortBy=popularity&apiKey=' + newsKey
+  response = requests.get(url)
+  json_data = json.loads(response.text)
+  news_data = json_data["articles"]
+  news_list = []
+  for index in range(news_limit):
   	news_list.append(news_data[index])
   return news_list
 
@@ -51,6 +66,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+  global news_limit
+
   if message.author == client.user:
       return
 
@@ -67,6 +84,15 @@ async def on_message(message):
     text = "\nHewwo!\nUseless bot made by udin.  \nMade with Python 🐍 \n"
     await message.channel.send(text)
 
+# Spam
+  if msg.startswith('/spam'):
+    splitted = msg.split()
+    name = splitted[1]
+    time = int(splitted[2])
+    for _ in range(time):
+      await message.channel.send(name)
+
+# Quran
   if msg.startswith('/quran'):
     quran_index = int(msg.split("/quran ", 1)[1]) - 1
     if quran_index < 114:
@@ -75,11 +101,33 @@ async def on_message(message):
     else:
       await message.channel.send("error lah blok, surah cuma ada 114")
 
+# News
   if msg.startswith('/news'):
-    news_list = getNews()
+    news_list = get_news()
     for news in news_list:
         text = news["source"]["name"] + " - " + news["description"]
         await message.channel.send(text + "\n===========")
+
+  if msg.startswith('/search'):
+    word = msg.split("/search ", 1)[1]
+    news_list = search_news(word)
+    await message.channel.send("SEARCHING FOR: " + word) 
+    for news in news_list:
+        text = news["source"]["name"] + " - " + news["description"]
+        await message.channel.send(text + "\n===========") 
+
+  if msg.startswith('/nl'):
+    await message.channel.send("News Limit: " + str(news_limit))
+
+  if msg.startswith('/sl'):
+    new_limit = int(msg.split("/sl ", 1)[1])
+    if (new_limit <= 0):
+      await message.channel.send("Use your brain")
+    elif (new_limit > 10):
+      await message.channel.send("Kebanyakan. memory bocor")
+    else:
+      news_limit = new_limit
+      await message.channel.send("Done. Current limit: " + str(new_limit))
 
   if any(word in msg for word in sad_words):
       await message.channel.send(random.choice(starter_encouragements))
